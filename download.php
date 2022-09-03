341e6ed45a3dfbc931d9b84a1671bd1854f3cadc
Vikas Patel <vikaspatel41800@gmail.com>
2022-09-04 00:03:01 +0530

    portfolio

---
+++ download.php
@@ -0,0 +1,9 @@
+<?php
+$file = $_GET['file'].".pdf";
+header("content-disposition: attachment; filename=" . urlencode($file));
+$fo = fopen($file, "r");
+while(!feof($fo)){
+    echo fread($fo, 8192);
+}
+fclose($fo);
+?>
